4fbcc5ad94c94aaa54ba7bb0b27410a17e04655e
dude3133 <dude33133@gmail.com>
2016-02-09 23:44:44 +0200

    TestController

---
+++ TheDarkSite/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+
+namespace TheDarkSite.Controllers
+{
+    public class TestController : ApiController
+    {
+        public int Get()
+        {
+            return 69;
+        }
+        public TestClass Get(int id)
+        {
+            return new TestClass
+            {
+                Id = id,
+                Smthn = "69!"
+            };
+        }
+    }
+
+    public class TestClass
+    {
+        public int Id { get; set; }
+        public string Smthn { get; set; }
+    }
+}
